1bc3d4776bcd3d22d862be55621be8257450fc2d
BigBull-H3RO <132926832+BigBull-H3RO@users.noreply.github.com>
2025-12-10 13:36:37 +0100

    REI Compatibility re-added

--- src/main/java/de/bigbull/vibranium/integration/REIPlugin.java
+++ src/main/java/de/bigbull/vibranium/integration/REIPlugin.java
@@ -1,32 +1,32 @@
-//package de.bigbull.vibranium.integration;
-//
-//import de.bigbull.vibranium.init.ItemInit;
-//import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
-//import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
-//import me.shedaniel.rei.api.common.util.EntryIngredients;
-//import me.shedaniel.rei.plugin.common.displays.brewing.DefaultBrewingDisplay;
-//import net.minecraft.world.item.Items;
-//
-//@me.shedaniel.rei.forge.REIPluginClient
-//public class REIPlugin implements REIClientPlugin {
-//    @Override
-//    public void registerDisplays(DisplayRegistry registry) {
-//        registry.add(new DefaultBrewingDisplay(
-//                EntryIngredients.of(Items.POTION),
-//                EntryIngredients.of(ItemInit.SOUL_HERB_MIXTURE.get()),
-//                EntryIngredients.of(ItemInit.SOUL_HERB_ELIXIR.get().getDefaultInstance())
-//        ));
-//
-//        registry.add(new DefaultBrewingDisplay(
-//                EntryIngredients.of(ItemInit.SOUL_HERB_ELIXIR.get()),
-//                EntryIngredients.of(Items.REDSTONE),
-//                EntryIngredients.of(ItemInit.SOUL_HERB_ELIXIR_EXTENDED.get().getDefaultInstance())
-//        ));
-//
-//        registry.add(new DefaultBrewingDisplay(
-//                EntryIngredients.of(ItemInit.SOUL_HERB_ELIXIR.get()),
-//                EntryIngredients.of(Items.GLOWSTONE_DUST),
-//                EntryIngredients.of(ItemInit.SOUL_HERB_ELIXIR_ENHANCED.get().getDefaultInstance())
-//        ));
-//    }
-//}
+package de.bigbull.vibranium.integration;
+
+import de.bigbull.vibranium.init.ItemInit;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.plugin.common.displays.brewing.DefaultBrewingDisplay;
+import net.minecraft.world.item.Items;
+
+@me.shedaniel.rei.forge.REIPluginClient
+public class REIPlugin implements REIClientPlugin {
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        registry.add(new DefaultBrewingDisplay(
+                EntryIngredients.of(Items.POTION),
+                EntryIngredients.of(ItemInit.SOUL_HERB_MIXTURE.get()),
+                EntryIngredients.of(ItemInit.SOUL_HERB_ELIXIR.get().getDefaultInstance())
+        ));
+
+        registry.add(new DefaultBrewingDisplay(
+                EntryIngredients.of(ItemInit.SOUL_HERB_ELIXIR.get()),
+                EntryIngredients.of(Items.REDSTONE),
+                EntryIngredients.of(ItemInit.SOUL_HERB_ELIXIR_EXTENDED.get().getDefaultInstance())
+        ));
+
+        registry.add(new DefaultBrewingDisplay(
+                EntryIngredients.of(ItemInit.SOUL_HERB_ELIXIR.get()),
+                EntryIngredients.of(Items.GLOWSTONE_DUST),
+                EntryIngredients.of(ItemInit.SOUL_HERB_ELIXIR_ENHANCED.get().getDefaultInstance())
+        ));
+    }
+}
